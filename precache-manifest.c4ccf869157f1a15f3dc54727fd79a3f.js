b7c4e0d212615f045cedf92cd79e04531d3d25fe
Tammy Metz <tmetz@github.com>
2020-06-30 09:36:52 -0400

    Updates

---
+++ precache-manifest.c4ccf869157f1a15f3dc54727fd79a3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b915b0bdba910b89ba7842aa01d19413",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "e7e39ae4904180413f20",
+    "url": "/test-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c493160e106fc38fd35a",
+    "url": "/test-react/static/js/2.57001fbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react/static/js/2.57001fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e39ae4904180413f20",
+    "url": "/test-react/static/js/main.6d80d9e5.chunk.js"
+  },
+  {
+    "revision": "749a69bc2f1e6da32b80",
+    "url": "/test-react/static/js/runtime-main.0b88a9d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
